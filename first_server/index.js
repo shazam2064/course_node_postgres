b7f8c0c1742df799c2796fe19c6e3d0691f1c865
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-09-21 05:33:52 -0400

    the first server index file was updated.

--- first_server/index.js
+++ first_server/index.js
@@ -4,23 +4,19 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const { url } = req;
+    const {url} = req;
+    console.log(url);
 
-  console.log(url);
-
-  if (url === '/translations') {
-    const translations = { 1: 'one', 2: 'two', 3: 'three' };
-
-    res.setHeader('Content-Type', 'application/json');
-
-    res.write(JSON.stringify(translations));
-
-    res.end();
-  }
-
-  res.end('Welcome to Node!');
+    if (url === '/translations') {
+        const translations = {1: 'one', 2: 'two', 3: 'three'};
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(translations));
+        res.end();
+    } else {
+        res.end('Welcome to Node!');
+    }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at ${hostname}:${port}`);
+    console.log(`Server running at ${hostname}:${port}`);
 });
