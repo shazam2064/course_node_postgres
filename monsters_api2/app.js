86b48f04403ffceb5d8a0814fc7e648aec0bf20f
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-10-02 05:07:58 -0400

    the monster router was added, and the app.js was updated.

--- monsters_api2/app.js
+++ monsters_api2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('./db')
 const {request, response} = require("express");
+const monsters = require('./routes/monsters');
 
 const app = express();
 
@@ -10,26 +11,10 @@ app.get('/', (req, res) => {
     })
 });
 
-app.get('/monsters', (request, response, next) => {
-    pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-        if (err) return next(err);
-
-        response.json(res.rows);
-    });
-});
-
-app.get('/monsters/:id', (request, response, next) => {
-    const {id} = request.params;
-
-    pool.query('SELECT * FROM monsters WHERE id = $1', [id], (err, res) => {
-        if (err) return next(err);
-
-        response.json(res.rows);
-    })
-})
+app.use('/monsters', monsters);
 
 app.use((err, req, res, next) => {
     res.json(err);
-})
+});
 
 module.exports = app;
