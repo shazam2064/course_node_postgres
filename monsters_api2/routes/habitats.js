dfd0ebdcf3e479692f215b86e47d92d78d85b5f3
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-10-03 05:35:58 -0400

    All the habitats endpoints were added.

--- monsters_api2/routes/habitats.js
+++ monsters_api2/routes/habitats.js
@@ -11,4 +11,65 @@ router.get('/', (request, response, next) => {
     });
 });
 
+router.get('/:id', (request, response, next) => {
+    const {id} = request.params;
+
+    pool.query('SELECT * FROM habitats WHERE id = $1', [id], (err, res) => {
+        if (err) return next(err);
+
+        response.json(res.rows);
+    })
+});
+
+router.post('/', (request, response, next) => {
+    const {name, climate, temperature} = request.body;
+
+    pool.query('INSERT INTO habitats(name,climate, temperature) VALUES($1, $2, $3)',
+        [name, climate, temperature],
+        (err, res) => {
+            if (err) return next(err);
+
+            response.redirect('/habitats');
+        }
+    );
+});
+
+router.put('/:id', (request, response, next) => {
+    const {id} = request.params;
+    const keys = ['name', 'climate', 'temperature'];
+    const fields = [];
+
+    keys.forEach(key => {
+        if (request.body[key]) fields.push(key);
+    });
+
+    fields.forEach((field, index) => {
+        pool.query(
+            `UPDATE habitats
+             SET ${field}=($1)
+             WHERE id = ($2)`,
+            [request.body[field], id],
+            (err, res) => {
+                if (err) return next(err);
+
+                if (index === fields.length - 1) {
+                    // response.redirect('/habitats');
+                    response.sendStatus(204);
+                }
+            }
+        )
+    });
+})
+
+router.delete('/:id', (request, response, next) => {
+    const {id} = request.params;
+
+    pool.query('DELETE FROM habitats WHERE id = $1', [id],
+        (err, res) => {
+            if (err) return next(err);
+
+            response.redirect('/habitats');
+        })
+});
+
 module.exports = router;
