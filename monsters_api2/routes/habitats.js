9e4a34d8d0b73dad58204660f5a62297f821d8ce
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-10-03 05:29:11 -0400

    the get habitats endpoint was added.

---
+++ monsters_api2/routes/habitats.js
@@ -0,0 +1,14 @@
+const {Router, response} = require('express');
+const pool = require('../db');
+
+const router = Router();
+
+router.get('/', (request, response, next) => {
+    pool.query('SELECT * FROM habitats ORDER BY id ASC', (err, res) => {
+       if (err) return next(err);
+
+       response.json(res.rows);
+    });
+});
+
+module.exports = router;
