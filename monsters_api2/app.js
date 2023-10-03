836e4fdadffe9884730ff640bc2e4090321a5238
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-10-03 05:42:48 -0400

    the get lives endpoint was added.

--- monsters_api2/app.js
+++ monsters_api2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const monsters = require('./routes/monsters');
 const habitats = require('./routes/habitats')
+const lives = require('./routes/lives')
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 
 app.use('/monsters', monsters);
 app.use('/habitats', habitats);
+app.use('/lives', lives);
 
 app.use((err, req, res, next) => {
     res.json(err);
---
+++ monsters_api2/routes/lives.js
@@ -0,0 +1,14 @@
+const {Router, request, response} = require('express');
+const pool = require('../db')
+
+const router = Router();
+
+router.get('/', (request, response, next) => {
+    pool.query('SELECT * FROM lives', (err, res) => {
+        if (err) return next(err);
+
+        response.json(res.rows);
+    });
+});
+
+module.exports = router;
